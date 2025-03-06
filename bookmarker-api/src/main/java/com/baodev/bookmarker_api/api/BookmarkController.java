9dabe04942b738abcd35b6a5a21da3b777fd5c73
Quang Bão <quangbao2307@gmail.com>
2025-03-06 22:08:43 +0700

    Use Paging, create init data

--- bookmarker-api/src/main/java/com/baodev/bookmarker_api/api/BookmarkController.java
+++ bookmarker-api/src/main/java/com/baodev/bookmarker_api/api/BookmarkController.java
@@ -2,12 +2,15 @@ package com.baodev.bookmarker_api.api;
 
 import com.baodev.bookmarker_api.domain.Bookmark;
 import com.baodev.bookmarker_api.domain.BookmarkService;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +20,11 @@ public class BookmarkController {
     private final BookmarkService bookmarkService;
 
     @GetMapping
-    public List<Bookmark> getAllBookmarks() {
-        return bookmarkService.getAllBookmarks();
+    public Page<Bookmark> getAllBookmarks(@Parameter(hidden=true) @PageableDefault(size = 10,
+                                                sort = "createdAt",
+                                                direction = Sort.Direction.DESC)
+                                             final Pageable pageable) {
+        return bookmarkService.getAllBookmarks(pageable);
     }
 
 }
--- bookmarker-api/src/main/java/com/baodev/bookmarker_api/domain/BookmarkService.java
+++ bookmarker-api/src/main/java/com/baodev/bookmarker_api/domain/BookmarkService.java
@@ -1,10 +1,10 @@
 package com.baodev.bookmarker_api.domain;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +13,8 @@ public class BookmarkService {
     private final BookmarkRepository bookmarkRepository;
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getAllBookmarks() {
-        return bookmarkRepository.findAll();
+    public Page<Bookmark> getAllBookmarks(Pageable pageable) {
+        return bookmarkRepository.findAll(pageable);
     }
 
 }
